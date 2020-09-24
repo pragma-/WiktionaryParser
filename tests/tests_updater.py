a81704ff2aa58ee990905590c3998008746d6364
Luca1152 <t.luca1152@gmail.com>
2020-09-24 19:41:44 +0300

    Add test updater
    Now there is a script to seamlessly update all the unit tests' output.

---
+++ tests/tests_updater.py
@@ -0,0 +1,20 @@
+from tests.test_core import *
+
+
+@mock.patch("requests.Session.get", side_effect=mocked_requests_get)
+def update_tests(mock_get):
+    fetch_languages = {}
+    pack_languages = {}
+    for tuple_word in test_words:
+        word, old_id, language = tuple_word
+        parser.set_language(language)
+        result = parser.fetch(word, old_id=old_id)
+        fetch_languages.setdefault(language, {}).update({word: result})
+        pack_languages.setdefault(language, {}).update({word: parser.pack_definitions_and_examples(result)})
+    with open('test_fetch_output.json', 'w') as f:
+        f.write(json.dumps(fetch_languages, ensure_ascii=False, indent=4))
+    with open('test_pack_output.json', 'w') as f:
+        f.write(json.dumps(pack_languages, ensure_ascii=False, indent=4))
+
+
+update_tests()
